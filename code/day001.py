9595ad25a201144fce541a674074b0491878f515
limetestlife <5685009350@qq.com>
2018-09-11 23:03:18 +0800

    代码

--- code/day001.py
+++ code/day001.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*-
 #Step 1: 导库 numpy,pandas
 import os
-
 import numpy as np
 import pandas as pd
-
+from sklearn.preprocessing import Imputer
 #Step 2: 导入数据
 rootpath = os.path.abspath('..')  # 获取上级路径
 dataset=pd.read_csv(rootpath+'\date\Data.csv')
@@ -18,5 +17,8 @@ Y = dataset.iloc[ : , 3].values
 print X
 print Y
 
-
-
+#处理缺失值
+imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
+imputer = imputer.fit(X[ : , 1:3])
+X[ : , 1:3] = imputer.transform(X[ : , 1:3])
+print X
