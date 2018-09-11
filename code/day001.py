93a588596dcf56727c202849501dff3fc69e3239
limetestlife <568509350@qq.com>
2018-09-11 20:08:17 +0800

    day01

--- code/day001.py
+++ code/day001.py
@@ -1,12 +1,22 @@
 # -*- coding: UTF-8 -*-
 #Step 1: 导库 numpy,pandas
+import os
+
 import numpy as np
 import pandas as pd
 
-#Step 2: Importing dataset
-dataset=pd.read_csv('D:\PyWork\machinelearing\date\Data.csv')
+#Step 2: 导入数据
+rootpath = os.path.abspath('..')  # 获取上级路径
+dataset=pd.read_csv(rootpath+'\date\Data.csv')
 print dataset
 
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , 3].values
+
+#TODO .values的作用
+
+print X
+print Y
+
+
 
---
+++ code/special/pandas.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+
+df =pd.DataFrame(np.random.randn(6,4),columns=list('ABCD'))  #创建多维数组
+df
+df.iloc[3]  #获取第4行
+type(df.iloc[3])   #得到 Series
+#切片操作
+df.iloc[3:5,0:2]   #获取4~5行，1~2列的数据
+df.iloc[[1,2,4],[0,2]]   #获取2，3，5行，1，2列的数据
+df.iloc[1:3,:]     #获取2~3行的所有列数据
+df.iloc[:,1:3]      #获取2~3列的所有行数据
+df.iloc[1,1]        #获取特定位置的值，功能等同与iat
+df.iat[1,1]         #比iloc更高效
+
+#TODO 为什么iat比iloc高效
+
